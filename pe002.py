6d16b45da4c466be7f5eb2ece0411ddc0c1aafef
Wesley Neill <wesley.neill@gmail.com>
2019-09-11 12:11:24 -0700

    formatting improved on exercise 2

--- pe002.py
+++ pe002.py
@@ -4,12 +4,11 @@ fib = [1, 2]
 
 result = 0
 
-while fib[len(fib) - 1]  + fib[len(fib) - 2] < 4e6:
-        fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
+while fib[len(fib) - 1] + fib[len(fib) - 2] < 4e6:
+    fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
 
 for i in fib:
     if i % 2 == 0:
         result += i
 
 print(result)
-
