cc0cbcf3ff414421f77c85ff438bad979e5af164
Wesley Neill <wesley.neill@gmail.com>
2019-09-11 12:09:40 -0700

    completed exercise 2

---
+++ pe002.py
@@ -0,0 +1,15 @@
+
+
+fib = [1, 2]
+
+result = 0
+
+while fib[len(fib) - 1]  + fib[len(fib) - 2] < 4e6:
+        fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
+
+for i in fib:
+    if i % 2 == 0:
+        result += i
+
+print(result)
+
