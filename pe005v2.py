c32d14f457137dcf457150b54b74c5ba54b9463b
Wesley Neill <wesley.neill@gmail.com>
2019-09-12 08:43:30 -0700

    Turned the algorithm for exercise 5 into a function for future use

--- pe005v2.py
+++ pe005v2.py
@@ -1,26 +1,60 @@
 import pe003 as pe
 from collections import Counter
 
-# A faster version for finding the least common multiple of all the numbers from 1 to 20
 
-prime_facts = {}
-lcm = 1
+def lcm(iterable):
+    """
+    A faster version for finding the least common multiple of all the
+    numbers from 1 to 20. My methodology here is based on the
+    Wikipedia article "Least Common Multiple". Therein two different
+    simple methods were described: 1.) Using the Greatest Common
+    Denominator and 2.) Using prime factorization. Since I peaked at
+    a solution found online using method 1, I decided to take a stab at
+    completing the exercise using method 2 on my own.
 
-for k in range(2, 21):
+    In simple terms, if one wants to know the least common multiple
+    of a set of numbers lcm(k_1, k_2, ... , k_n), one need only find
+    the prime factorization of each k_i, and count the highest power of
+    each recorded primes. Once the highest power of each prime is found,
+    We multiply each prime to its highest found power to get our answer
 
-    kfacts = pe.prime_factors(k)
-    print("factorization of", k, "is", kfacts)
+    EXAMPLE: We want to know lcm(2,4,5,6,8)
 
-    for i in kfacts:
-        count = Counter(kfacts)[i]
+    Prime Factorizations of each k_i:
+    2 -> 2        (2^1)
+    4 -> 2, 2     (2^2)
+    5 -> 5        (5^1)           *** highest power of 5
+    6 -> 2, 3     (2^1), (3^1)    *** highest power of 3
+    8 -> 2, 2, 2  (2^3)           *** highest power of 2
 
-        if i not in prime_facts.keys():
-            prime_facts[i] = count
+    Therefore, lcm(2, 4, 5, 6, 8) = 2^3 * 3^1 * 5^1 = 120
 
-        elif i in prime_facts.keys() and prime_facts[i] < count:
-            prime_facts[i] = count
+    :param iterable: the set of integers you wish to find the LCM of
+    :return: the LCM of all numbers in the iterable parameter
+    """
+    prime_facts = {}
+    lcm = 1
 
-for f in prime_facts.keys():
-    lcm = lcm * f**prime_facts[f]
+    for k in iterable:
 
-print(lcm)
+        kfacts = pe.prime_factors(k)
+        print("factorization of", k, "is", kfacts)
+
+        for i in kfacts:
+            count = Counter(kfacts)[i]
+
+            if i not in prime_facts.keys():
+                prime_facts[i] = count
+
+            elif i in prime_facts.keys() and prime_facts[i] < count:
+                prime_facts[i] = count
+
+    for f in prime_facts.keys():
+        lcm = lcm * f**prime_facts[f]
+
+    return lcm
+
+
+if __name__ == '__main__':
+
+    print(lcm([8,9,21]))
