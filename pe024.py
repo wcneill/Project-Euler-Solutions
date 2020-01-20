dfc27dcd3fb21645e61ff2355e3de5642e86d837
Wesley Neill <wesley.neill@gmail.com>
2020-01-20 14:30:29 -0800

    completed PE24

--- pe024.py
+++ pe024.py
@@ -1,18 +1,20 @@
 
 def lexi_order(nums):
 
-    permutations = []
-    print(nums)
+    permutations = [nums]
     length = len(nums)
 
     while True:
+
+        nums = list(nums)
+
+        # find largest index i such that nums[i] < nums[i + 1]
         exists = False
         for j, elem in enumerate(nums):
             # check if last element
             if j == length - 1:
                 break
 
-            # find largest index i such that nums[i] < nums[i + 1]
             if elem < nums[j + 1]:
                 i = j
                 exists = True
@@ -31,11 +33,12 @@ def lexi_order(nums):
         # reverse order of elements starting at position i+1
         to_reverse = nums[i+1:][::-1]
         nums[i+1::] = to_reverse
-        print(nums)
+
         permutations.append(nums)
+
     return permutations
 
 
 if __name__ == '__main__':
-    perms = lexi_order([1,2,3])
-    print(perms)
+    perms = lexi_order([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    print(perms[999999])
